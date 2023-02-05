ce865d2efab2c01bd3a02d421702e8ece360cfa7
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-02-05 18:34:55 +0300

    Refactor main

--- src/main.asm
+++ src/main.asm
@@ -1,20 +1,29 @@
+default rel
+
+section .rodata
+    message db 'Hello, world!', 10
+    MESSAGE_LEN equ $ - message
 
 section .text
     global _start
 
-_start:  ; tells linker entry point
+print_message:
     mov edx, MESSAGE_LEN  ; message length
     mov ecx, message  ; message to write
-    mov ebx, 1  ; file descriptor (stdout)
     mov eax, 4  ; system call number (sys_write)
+    mov ebx, 1  ; file descriptor (stdout)
     int 0x80  ; call kernel
-    mov eax, 1  ; system call number (sys_exit)
-    mov ebx, 0  ; return value
-    int 0x80   ; call kernel
     ret
 
-section .data
-    message db 'Hello, world!', 0
+_start:
+    call print_message
+    call _exit
 
-    MESSAGE_LEN equ $ - message
+_exit:
+    ; mov eax, 1  ; system call number (sys_exit)
+    ; mov ebx, 0  ; return value
+    ; int 0x80   ; call kernel
+    mov eax, 60  ; __NR_exit call number (x86_64)
+    xor edi, edi ; status = 0 (exit normally)
+    syscall      ; _exit(0)
 
